ca9aca7a61f6b27ec845eca28a14b9f4094bb107
h0ngwon <w___on@naver.com>
2023-11-17 20:52:13 +0900

    Chore: redux devtools extensions 설치

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,9 +1,10 @@
 import { createStore } from "redux";
 import { combineReducers } from "redux";
+import { composeWithDevTools } from '@redux-devtools/extension';
 import workoutReducer from "redux/reducers/workout";
 import menuReducer from "redux/reducers/menu";
 
 const rootReducer = combineReducers({ workoutReducer, menuReducer });
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools());
 
 export default store;
